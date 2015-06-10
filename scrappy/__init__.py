776aa630c4417db33ff557203123d4283f5af640
cmccully <cmccully@physics.rutgers.edu>
2015-06-10 15:37:25 -0700

    updated init file.

--- scrappy/__init__.py
+++ scrappy/__init__.py
@@ -12,4 +12,7 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from .scrappy import detect_cosmics
+    from .scrappy import *
+    from .utils import *
+
+__all__ = ['detect_cosmics']
