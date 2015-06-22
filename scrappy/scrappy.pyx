a8c5347ea40e6194f72e8a4c8bc176ad19202868
cmccully <cmccully@physics.rutgers.edu>
2015-06-22 11:05:15 -0700

    Updated docstring and removed data type requirement for input data.

--- scrappy/scrappy.pyx
+++ scrappy/scrappy.pyx
@@ -25,13 +25,12 @@ ctypedef uint8_t bool
 
 from libc.stdlib cimport malloc, free
 
-def detect_cosmics(np.ndarray[np.float, ndim=2, mode='c', cast=True] indat,
-                   np.ndarray[np.uint8_t, ndim=2, mode='c', cast=True] inmask=None,
-                   float sigclip=4.5, float sigfrac=0.3, float objlim=5.0,
-                   float gain=1.0, float readnoise=6.5, float satlevel=65536.0,
-                   float pssl=0.0, int niter=4, sepmed=True, cleantype='meanmask',
-                   fsmode='median', psfmodel='gauss', float psffwhm=2.5,
-                   int psfsize=7, psfk=None, float psfbeta=4.765, verbose=False):
+def detect_cosmics(indat, inmask=None, float sigclip=4.5, float sigfrac=0.3,
+                   float objlim=5.0, float gain=1.0, float readnoise=6.5,
+                   float satlevel=65536.0, float pssl=0.0, int niter=4,
+                   sepmed=True, cleantype='meanmask', fsmode='median',
+                   psfmodel='gauss', float psffwhm=2.5, int psfsize=7,
+                   psfk=None, float psfbeta=4.765, verbose=False):
     """detect_cosmics(indat, inmask=None, sigclip=4.5, sigfrac=0.3, objlim=5.0,
                       gain=1.0, readnoise=6.5, satlevel=65536.0, pssl=0.0,
                       niter=4, sepmed=True, cleantype='meanmask',
@@ -43,10 +42,10 @@ def detect_cosmics(np.ndarray[np.float, ndim=2, mode='c', cast=True] indat,
     https://github.com/astropy/astro-scrappy
 
     Please cite the original paper which can be found at:
-    U{http://www.astro.yale.edu/dokkum/lacosmic/}
+    http://www.astro.yale.edu/dokkum/lacosmic/
 
     van Dokkum 2001, PASP, 113, 789, 1420
-    (article : U{http://adsabs.harvard.edu/abs/2001PASP..113.1420V})
+    (article : http://adsabs.harvard.edu/abs/2001PASP..113.1420V)
 
     Parameters
     ----------
