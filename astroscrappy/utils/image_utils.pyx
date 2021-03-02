5433134f5bd49669c5dfb490755f37ab57885022
Simon Conseil <contact@saimon.org>
2021-03-02 17:00:12 -0300

    Cython needs directives on one line

--- astroscrappy/utils/image_utils.pyx
+++ astroscrappy/utils/image_utils.pyx
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-# cython: boundscheck=False, nonecheck=False, wraparound=False
-# cython: language_level=3, cdivision=True
+# cython: boundscheck=False, nonecheck=False, wraparound=False, language_level=3, cdivision=True
 """
 Name : image_utils
 Author : Curtis McCully
