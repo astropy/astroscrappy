54dc3a4b1aa41ee72fd1b45f0cbc6aa8586f3c8c
Simon Conseil <s.conseil@ip2i.in2p3.fr>
2024-04-05 18:49:12 +0200

    Move setup.cfg to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# NOTE: The configuration for the package, including the name, version, and
-# other information are set in the setup.cfg file.
 
 import os
 
